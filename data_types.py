22b4f18bc186860b9afae1505843fbaa877cff23
kamiprok <kamiprok@gmail.com>
2018-12-24 12:42:00 +0100

    first entry

---
+++ data_types.py
@@ -0,0 +1,83 @@
+# datatypes: int, float, str, list, dict, tup, set, bool
+
+# basic math operations
+
+2+2
+2-1
+2*2
+3/2
+
+# mod modulo
+
+7%4
+50%5
+23%2
+
+# powers
+
+2**3
+
+# order of ops
+
+2 + 10 * 10 + 3
+
+
+# variables
+
+a = 5
+a = 10
+a + a
+a = a + a
+type(a)
+a = 30.1
+type(a)
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+
+
+# strings
+
+'hello'
+"hello"
+'this is also a string'
+"I'm going for a run"
+print('hello')
+"Hello world one"
+'hello world two'
+print('hello world one')
+print('hello world two')
+print('hello \nworld')
+print('hello \tworld')
+len('hello')
+len('I am')
+
+
+# string indexing and slicing
+
+mystring = 'hello world'
+mystring[0]
+mystring[8]
+mystring[9]
+mystring[-2]
+
+mystring = 'abcdefghijk'
+mystring[2:]
+mystring[:3]
+mystring[3:6]
+mystring[1:3]
+
+mystring[::2]
+mystring[2:7:2]
+mystring[::-1]
+
+# strings are immutable
+
+name = 'Sam'
+# name[0] = 'P'  nie da sie
+last_letters = name[1:]
+'P' + last_letters
+# string concattination
+x = 'hello world'
+x + ' it is beautiful day'
+
